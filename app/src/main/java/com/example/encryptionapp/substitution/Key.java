183ab50ce4301058077da47857314d0b807592bf
healym3 <79485702+healym3@users.noreply.github.com>
2022-03-08 13:12:32 -0600

    update key

--- app/src/main/java/com/example/encryptionapp/substitution/Key.java
+++ app/src/main/java/com/example/encryptionapp/substitution/Key.java
@@ -1,4 +1,20 @@
 package com.example.encryptionapp.substitution;
 
 public class Key {
+    private String key;
+
+    private boolean checkKey(String key){
+
+        return true;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public boolean setKey(String key) {
+        this.key = key;
+
+        return true;
+    }
 }
