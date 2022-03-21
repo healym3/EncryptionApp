2078baf99b5c505ac8830b4b527049da42df3ece
healym3 <healym3@gator.uhd.edu>
2022-03-21 12:17:43 -0500

    minor update

--- app/src/main/java/com/example/encryptionapp/data/Quadgram.java
+++ app/src/main/java/com/example/encryptionapp/data/Quadgram.java
@@ -2,12 +2,7 @@ package com.example.encryptionapp.data;
 
 import java.util.List;
 
-//"alphabet": "abcdefghijklmnopqrstuvwxyz",
-//"nbr_quadgrams": 93609337,
-//"most_frequent_quadgram": "tion",
-//"max_fitness": 1358,
-//"average_fitness": 214.8411689016491,
-//"quadgrams": [
+
 public class Quadgram {
     String alphabet;
     int numberQuadgrams;
